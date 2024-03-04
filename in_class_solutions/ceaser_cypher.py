76e9d2f7b3a581c5b8abdf822ca18fdbb430ad23
VincentQu888 <112832158+VincentQu888@users.noreply.github.com>
2024-03-04 10:32:53 -0500

    Update ceaser_cypher.py

--- in_class_solutions/ceaser_cypher.py
+++ in_class_solutions/ceaser_cypher.py
@@ -1,14 +1,20 @@
 #Vincent Qu, 341089043
 
+def ceaser_cypher(word, shift):
+    cypher = []
+    ans = []
+    for i in range(2):
+        for j in range(26):
+            cypher.append(chr(j + ord('a')))
+
+    for char in word:
+        ans.append(cypher[ord(char) - ord('a') + shift])
+    return "".join(ans)
+
+
 inp = input()
 word, temp_shift = inp.split()
 word = word[0 : len(word)-1].lower()
 shift = int(temp_shift)%26
 
-cypher = []
-for i in range(2):
-    for j in range(26):
-        cypher.append(chr(j + ord('a')))
-
-for char in word:
-    print((cypher[ord(char) - ord('a') + shift]), end = "")
+print(ceaser_cypher(word, shift))
